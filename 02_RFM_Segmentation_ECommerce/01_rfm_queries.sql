0475de3e79a866a8e0e0eaa3f3492e5975779720
Damian Dawidek <ddawidek366@icloud.com>
2026-09-17 20:35:44 +0200

    Update 01_rfm_queries.sql

--- 02_RFM_Segmentation_ECommerce/01_rfm_queries.sql
+++ 02_RFM_Segmentation_ECommerce/01_rfm_queries.sql
@@ -59,12 +59,25 @@ with rfm as (
 ), order_dates as (
 	select max(oo.order_purchase_timestamp::date) as order_dayes
 	from olist_orders oo
-)
+), raw_rfm as (
 
 select  
     rfm.customer_unique_id,
     ( od.order_dayes - rfm.customer_dayes ) as recency_days,
     rfm.total_orders as frequency,
     rfm.total_amount as monetary
+
 from rfm
 cross join order_dates od
+)
+
+select  
+    rr.customer_unique_id,
+    rr.recency_days,
+    rr.frequency,
+    rr.monetary,
+    ntile(5) over (order by recency_days desc ) AS r_score,
+    ntile(5) over (order by frequency desc ) AS f_score,
+    ntile(5) over (order by monetary desc ) AS m_score
+
+from raw_rfm as rr
